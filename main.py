64d594dc933b4e94b883b47620c4bbf92607f383
vigandika <44347567+vigandika@users.noreply.github.com>
2020-08-14 00:49:46 +0200

    Ready to execute

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from examples import *
 from puzzle import EightPuzzle
 
 if __name__ == '__main__':
-    # EightPuzzle(puzzle_5).solve_8puzzle('misplaced_tiles')
-    EightPuzzle(unsolvable_1).solve_8puzzle('manhattan_distance')
+    EightPuzzle(puzzle_5).solve_8puzzle('misplaced_tiles')
+    # EightPuzzle(puzzle_5).solve_8puzzle('manhattan_distance')
     # EightPuzzle(EightPuzzle.random_puzzle_generator()).solve_8puzzle('manhattan_distance')
